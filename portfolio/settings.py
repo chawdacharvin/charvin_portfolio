c6a24dbcea2ea876f209fd0779d63a499792fbf5
chawdacharvin <118664115+chawdacharvin@users.noreply.github.com>
2025-03-28 17:38:18 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -102,7 +102,7 @@ STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL= '/media/'
-MEDIA_ROOT= BASE_DIR/ 'media'
+MEDIA_ROOT= os.path.join(BASE_DIR/ 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
